19efb4d2b64fd7420175f7139685c72d523c0359
Ryder Anderson <ra302211@gmail.com>
2020-07-28 10:52:24 -0500

    Added main menu

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 
 namespace DragonBlockAdventure_1
 {
-    public enum GAME_STATE{NONE, MENU, BATTLE, TRAINING}
+    public enum GAME_STATE{NONE, MENU, BATTLE, TRAINING, STORY}
     public enum BATTLE_STATE{NONE, PLAYER_TURN, ENEMY_TURN, BEAM_STRUGGLE}  // Not sure how I want battles to go 100%
     class Program
     {
@@ -25,7 +25,7 @@ namespace DragonBlockAdventure_1
 
         // Game Methods
         public static void StartGame(){
-            
+            Console.Clear();
             Console.WriteLine("Welcome to Dragon Block C Adventure!");
             Console.WriteLine("DBC: Adventure! is all about training to get stronger and defeating the enemies in your way to completing your goal.");
             NewCharacter();
@@ -35,7 +35,43 @@ namespace DragonBlockAdventure_1
         public static void MainMenu(){  // Show main menu screen
             Console.Clear();
             state = GAME_STATE.MENU;
-            Console.WriteLine("Menu Coming soon!");  // Need to design the menu
+            ShowStats();
+            Console.WriteLine("\nType in the number of the option you want to select");  // Had a weird bug in vs code where it would show option 1&2 twice before stats
+            Console.WriteLine("1.Story");
+            Console.WriteLine("2.Training");
+            Console.WriteLine("3.Options");
+            Console.WriteLine("4.Exit");
+
+            int selection = Convert.ToInt16(Console.ReadLine());
+            switch(selection){
+                case 1:
+                    Console.WriteLine("Coming Soon!");
+                    Console.WriteLine("Press Enter to continue.");
+                    Console.ReadKey();
+                    MainMenu();
+                    break;
+                case 2:
+                    Console.WriteLine("Coming Soon!");
+                    Console.WriteLine("Press Enter to continue.");
+                    Console.ReadKey();
+                    MainMenu();
+                    break;
+                case 3:
+                    Console.WriteLine("Coming Soon!");
+                    Console.WriteLine("Press Enter to continue.");
+                    Console.ReadKey();
+                    MainMenu();
+                    break;
+                case 4:
+                    Console.WriteLine("Goodbye!");
+                    break;
+                default:
+                    Console.WriteLine("Please choose an option");
+                    Console.WriteLine("Press Enter to continue.");
+                    Console.ReadKey();
+                    MainMenu();
+                    break;
+            }
         }
         
         // Player Methods
@@ -43,7 +79,19 @@ namespace DragonBlockAdventure_1
             Console.WriteLine("What will your character's name be?");
             string charName = Console.ReadLine();
             Player.Name = charName;
+            Player.level = 1;
             Player.RestoreCharacter();
+        }
+
+        static void ShowStats(){
+            Console.WriteLine(Player.Name + "LVL:" + Player.level);
+            Console.WriteLine("HP: " + Player.health + "/" + Player.maxHealth);
+            Console.WriteLine("KI: " + Player.ki + "/" + Player.maxKi);
+            Console.WriteLine("STAMINA: " + Player.stamina + "/" + Player.maxStamina);
+            Console.WriteLine("Attack:" + Player.attack);
+            Console.WriteLine("KiAttack:" + Player.kiAttack);
+            Console.WriteLine("Defence:" + Player.defence);
+            Console.WriteLine("Speed:" + Player.speed);
         }
 
         // Dialog
